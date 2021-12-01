baf196134db0f8a3eb4e022bd231645559ef9194
Leonardo Fontana <fontana.leonardo@gmail.com>
2021-12-01 12:51:09 +0100

    wip

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,65 @@
-import Generator = require('yeoman-generator');
+import Generator = require("yeoman-generator");
 
-module.exports = class extends Generator {
-    constructor(args, opts) {
-        super(args, opts);
-    
-        // Next, add your custom code
-        this.option('babel'); // This method adds support for a `--babel` flag
-      }
-      method1() {
-        this.log('method 1 just ran');
-      }
-    
-      method2() {
-        this.log('method 2 just ran');
-      }
+interface IAnswers {
+  name: string;
+  author: string;
+  title: string;
+  language: string;
+  publisher: string;
+}
+export default class extends Generator {
+  answers: IAnswers = {
+    name: '',
+    author: '',
+    title: '',
+    language: '',
+    publisher: '',
+  };
+
+  constructor(args, opts) {
+    super(args, opts);
+
+    // Next, add your custom code
+    this.option("babel"); // This method adds support for a `--babel` flag
+  }
+
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname, // Default to current folder name
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "Book author?",
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Book title?",
+      },
+      {
+        type: "input",
+        name: "language",
+        message: "Book language?",
+      },
+      {
+        type: "input",
+        name: "publisher",
+        message: "Book publisher?",
+      },
+    ]);
+  }
+
+  writing() {
+    const options = this.answers;
+    this.fs.copyTpl(
+      this.templatePath('content.opf'),
+      this.destinationPath('public/content.opf'),
+      options
+    );
+  }
 };
--- tests/test.ts
+++ tests/test.ts
@@ -1,9 +1,29 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import helpers from "yeoman-test";
+import f from "../src/app";
 
-describe('ccc', () => {
-    it("test", () =>{
-        expect(true).to.be.true;
-    })
+describe("ccc", () => {
+  let runResult;
+  beforeEach(async () => {
+    runResult = await helpers
+      .create("../src/app", {}, {})
+      .withPrompts({
+        author: "author",
+        title: "title",
+        name: "name",
+      }) // Mock the prompt answers
+      .run();
+  });
+  afterEach(() => {
+    if (runResult) {
+      runResult.restore();
+    }
+  });
 
-}
-)
+  it("test", () => {
+    expect(true).to.be.true;
+  });
+  it("test yeo", () => {
+    runResult.assertNoFile('file.txt');
+  });
+});
